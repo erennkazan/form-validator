8335030c5b30f9e0a9e432497df9cf62d449f303
Eren Kazan <erennkazan@gmail.com>
2023-02-11 19:12:30 +0300

    form validate added

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+function validateForm(){
+    isValid = form.checkValidity();
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+}   
+
+function prossesFormData(e){
+    validateForm();
+    e.preventDefault();
+}
+
+form.addEventListener("submit", prossesFormData);
