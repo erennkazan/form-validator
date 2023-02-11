d08f90aee651c2e2998147bea23b822b6a50202c
Eren Kazan <erennkazan@gmail.com>
2023-02-11 19:23:23 +0300

    password match added

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm(){
     isValid = form.checkValidity();
-    message.textContent = 'Please fill out all fields.';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
+    if(!isValid){
+        message.textContent = 'Please fill out all fields.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+    }
+    if(password1El.value === password2El.value){
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+    }else{
+        passwordsMatch = false;
+        message.textContent = 'Make sure passwords match.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+    }
+    if(isValid && passwordsMatch){
+        message.textContent = 'Successfully Registered!';
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+
+    }
 }   
 
 function prossesFormData(e){
